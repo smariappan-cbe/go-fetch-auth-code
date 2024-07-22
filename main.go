f7176e7d9bd27ae7d305035e8e4c010815c344cd
Fabrizio Machado <fabrizio@akoya.com>
2024-07-22 10:44:41 -0400

    Basic golang sample to fetch auth code

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+const (
+	// TODO replace below sandbox app creds with your own obtained from recipient hub
+	ClientId = "<paste your client id here>"
+)
+
+func main() {
+	r := gin.New()
+	r.Use(
+		gin.LoggerWithWriter(gin.DefaultWriter, "/"),
+		gin.Recovery(),
+	)
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title":            "Getting an auth code",
+			"authCodeRedirect": fmt.Sprintf("https://sandbox-idp.ddp.akoya.com/auth?connector=mikomo&response_type=code&client_id=%s&redirect_uri=https://recipient.ddp.akoya.com/flow/callback&scope=openid&offline_access&state=appstate", ClientId),
+		})
+	})
+	r.Run(":8123")
+}
---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Step 1: Download all dependencies listed in go.mod
+go mod tidy
+go mod vendor
+
+# Step 2: Build the main.go file
+go build -o main .
+
+# Step 3: Run the main executable
+./main
